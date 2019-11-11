839a7112955eda1129e06bc0b6f1b12119be8716
Kenneth Cho <nykid99@gmail.com>
2019-11-10 22:37:36 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # zip.sh
 # author
-echo "Enter a vaild username: "
-read ZIP
-while echo "$ZIP" | egrep "^[a-z][0-9a-z_]{2-11}$" > /dev/null 2>&1
+echo "Enter a vaild username. Only lower case letters, digits and the underscore characters can be used. Must start with lower case letter, and must contain at least three but no more than 12 characters"
+read USRNAME
+while echo "$USRNAME" | egrep "^[a-z][0-9a-z_]{2-11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a valid username: "
-	read ZIP
+	read USRNAME
 done
 echo "Thank you"
